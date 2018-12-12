35aba5d6e3096fb0775df48118efd17a193e0842
Shivetya <gthuillier2@gmail.com>
2018-12-12 12:10:43 +0100

    Add text in TextViews in activity_history.xml

--- app/src/main/java/com/thuillier/guillaume/moodtracker/controller/HistoryActivity.java
+++ app/src/main/java/com/thuillier/guillaume/moodtracker/controller/HistoryActivity.java
@@ -2,13 +2,34 @@ package com.thuillier.guillaume.moodtracker.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import com.thuillier.guillaume.moodtracker.R;
 
-public class HistoryActivity extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTv1, mTv2, mTv3, mTv4, mTv5, mTv6, mTv7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        mTv1 = findViewById(R.id.history_activity_first_TV);
+        mTv2 = findViewById(R.id.history_activity_second_TV);
+        mTv3 = findViewById(R.id.history_activity_third_TV);
+        mTv4 = findViewById(R.id.history_activity_fourth_TV);
+        mTv5 = findViewById(R.id.history_activity_fifth_TV);
+        mTv6 = findViewById(R.id.history_activity_sixth_TV);
+        mTv7 = findViewById(R.id.history_activity_seventh_TV);
+    }
+
+    private void verySad(TextView tv){
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
